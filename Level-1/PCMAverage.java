6a916de6042843eeb39931ff26347674b4e4e833
veera-raghavan-officiall <veera.veeraraghavanofficial@gmail.com>
2026-03-11 08:56:09 +0530

    Force fix folder structure

--- Level-1/PCMAverage.java
+++
@@ -1,7 +0,0 @@
-class PCMAverage {
-    public static void main(String[] args) {
-        int maths = 94, physics = 95, chemistry = 96; // [cite: 71]
-        double average = (maths + physics + chemistry) / 3.0;
-        System.out.println("Sam’s average mark in PCM is " + average); // [cite: 73]
-    }
-}
